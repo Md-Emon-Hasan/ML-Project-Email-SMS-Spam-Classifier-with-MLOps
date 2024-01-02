b9174fc32f8af4b0603538e9fc1ab81429c0501e
Md-Emon-Hasan <iconicemon01@gmail.com>
2024-01-02 23:32:14 +0600

    me

--- app.py
+++ app.py
@@ -1,57 +1,50 @@
 import streamlit as st
 import pickle
+import nltk
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
+    
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-
     text = y[:]
     y.clear()
 
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-
     text = y[:]
     y.clear()
 
     for i in text:
         y.append(ps.stem(i))
-
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-# tfidf = pickle.load(open('C:/Users/emon1/OneDrive/Desktop/sms-spam-classifier-main/vectorizer.pkl','rb'))
+st.title('Email & SMS Spam Classifier\n Made By Emon Hasan')
 
-model = pickle.load(open('model.pkl','rb'))
-# model = pickle.load(open('C:/Users/emon1/OneDrive/Desktop/sms-spam-classifier-main/model.pkl','rb'))
+tfidf = pickle.load(open('C:/Users/emon1/OneDrive/Desktop/test/vectorizer.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
+model = pickle.load(open('C:/Users/emon1/OneDrive/Desktop/test/model.pkl','rb'))
 
-input_sms = st.text_area("Enter the message")
+input_sms = st.text_input('Enter the Email/Message:')
 
-if st.button('Predict'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+if st.button('Click for Predict'):
+    # 1. Preprocess
+    transform_sms = transform_text(input_sms)
+    # 2. vecotorize
+    vector_input = tfidf.transform([transform_sms])
+    # 3. Predict
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
-        st.header("Spam")
+        st.header('Spam')
     else:
-        st.header("Not Spam")
+        st.header('Ham')
